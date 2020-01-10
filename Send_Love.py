fab0b21f3371c569a566330475eb3266a50e1cc5
cuijianzhe <48144475+cuijianzhe@users.noreply.github.com>
2020-01-10 13:50:52 +0800

    Update Send_Love.py

--- Send_Love.py
+++ Send_Love.py
@@ -10,45 +10,42 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 import datetime
-import time
 import random
 import linecache
 def symbol(file):
     fuhao = ["(￣▽￣)~*","(￣▽￣)／","(〃'▽'〃)","ヾ(ｏ･ω･)ﾉ","(￣.￣)","ヾ(@^▽^@)ノ","(｡◕ˇ∀ˇ◕)","(￢_￢)瞄","＾＿－)≡★ ","✧(＾＿－✿ ","｡◕ᴗ◕｡","ლ(⁰⊖⁰ლ)","(´•༝•`)","( ･´ω`･ )","(*≧▽≦)","(´･ᴗ･`)"]
     # fuhao = ['▲','▼','●','◆','■','★','▶','◀','♥','♦','$','%','@','&']
-    huashu = ["❤ 爱你的哲哥 ❤","❤ L love you ❤","❤ jialijuan，你是我的宝宝 ❤","❤ 我不管，你就是我的人了 ❤","❤ 著名奶茶鉴定家 ❤","❤ 退役魔法少女 ❤","首席漂亮官","当地比较可爱的一类人","哲哥要娶你"]
-    date = time.strftime('%Y-%m-%d',time.localtime())
+    huashu = ["❤ 爱你的哲哥 ❤","❤ L love you ❤","❤ 贾丽娟，你是我的宝宝 ❤","❤ 我不管，你就是我的人了 ❤","❤ 著名奶茶鉴定家 ❤","❤ 退役魔法少女 ❤","首席漂亮官","当地比较可爱的一类人","哲哥要娶你"]
+    date = datetime.datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
     starttime = datetime.datetime(2019,12,16)
     endtime = datetime.datetime.now()
     times = (endtime - starttime).days
     with open(file,encoding='utf-8') as yuju:
-        qinghua = linecache.getline('wenben', random.randint(1, len(yuju.readlines()))).split('、')[1].strip()
+        qinghua = linecache.getline(file, random.randint(1, len(yuju.readlines()))).split('、')[1].strip()
     word = random.choice(huashu)
     hongxin = (random.choice(fuhao)) * 8
     body = """
     Dear LiJuan：
     
-         %s
-         
-         ❤ 今天是%s，是我和你在一起的第%s天，我至死不渝的爱你……   ❤
-         ❤ 今日份小甜甜：%s
-
-         %s
-         
+         %s\n
+         ❤\t 呃，%s，I miss you again !
+         ❤\t今天是我和你在一起的第%s天，我至死不渝的爱你……   ❤
+         ❤\t今日份小甜甜：%s \n
+         %s\n
          ❤ %s ❤
     """%(hongxin,date,times,qinghua,hongxin,word)
     return body
 def _email(content):
-    my_sender = '22222222@qq.com'
+    my_sender = '598941324@qq.com'
     my_pass = 'mypassword'
-    my_user = '1111111111@qq.com'
+    my_user = ['cuijianzhe@limikeji.com','5989413240@qq.com']
     msg = MIMEText(content, 'plain', 'utf-8')
     msg['From'] = formataddr(["爱你的哲", my_sender])
-    msg['To'] = formataddr(["小娟娟", my_user])
+    msg['To'] = ','.join(my_user)
     msg['Subject'] = '更新爱你的日期到永远'
     server = smtplib.SMTP_SSL("smtp.qq.com", 465)
     server.login(my_sender, my_pass)
-    server.sendmail(my_sender, [my_user], msg.as_string())
+    server.sendmail(my_sender, msg['To'].split(','), msg.as_string())
     server.quit()
 if __name__ == '__main__':
     text = symbol('/shell/love/wenben')
